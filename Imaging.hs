317f3fa78d2b2675b30a1dc7468769c1a3375f6e
ForNeVeR <neverthness@gmail.com>
2012-10-03 22:58:11 +0700

    Imaging: file uploading.

--- Imaging.hs
+++ Imaging.hs
@@ -1,14 +1,22 @@
 module Imaging where
 
+import System.Directory (renameFile)
 import Happstack.Server (Browsing(DisableBrowsing),
                          Response,
                          ServerPart,
+                         lookFile,
                          ok,
                          serveDirectory,
                          toResponse)
 
+dataDirectory :: [Char]
+dataDirectory = "./data"
+
 addImage :: ServerPart Response
-addImage = ok $ toResponse "Ok"
+addImage = do file <- lookFile "image"
+              liftIO $ moveToDataDirectory file
+              ok $ toResponse "Ok"
+           where moveToDataDirectory (filename, _, _) = renameFile filename (dataDirectory ++ "/" ++ filename)
 
 viewImage :: ServerPart Response
-viewImage = serveDirectory DisableBrowsing [] "./data"
+viewImage = serveDirectory DisableBrowsing [] dataDirectory
