2fccd0a16701f3d501e17774419020a0581bf19f
ForNeVeR <neverthness@gmail.com>
2012-10-04 21:32:02 +0700

    Imaging: uploading.

--- Imaging.hs
+++ Imaging.hs
@@ -1,6 +1,9 @@
 module Imaging where
 
+import Control.Monad.Trans (liftIO)
 import System.Directory (renameFile)
+import System.FilePath (combine, takeFileName)
+import Text.Blaze ((!))
 import Happstack.Server (Browsing(DisableBrowsing),
                          Response,
                          ServerPart,
@@ -9,14 +12,26 @@ import Happstack.Server (Browsing(DisableBrowsing),
                          serveDirectory,
                          toResponse)
 
-dataDirectory :: [Char]
-dataDirectory = "./data"
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 
-addImage :: ServerPart Response
-addImage = do file <- lookFile "image"
-              liftIO $ moveToDataDirectory file
-              ok $ toResponse "Ok"
-           where moveToDataDirectory (filename, _, _) = renameFile filename (dataDirectory ++ "/" ++ filename)
+addImage :: String -> ServerPart Response
+addImage dataDirectory = do file <- lookFile "image"
+                            liftIO $ moveToDataDirectory file
+                            ok $ toResponse "Ok"
+                         where moveToDataDirectory (filepath, _, _) = let filename = (takeFileName filepath)
+                                                                      in  renameFile filepath $ dataDirectory `combine` filename
 
-viewImage :: ServerPart Response
-viewImage = serveDirectory DisableBrowsing [] dataDirectory
+addImageWeb :: ServerPart Response
+addImageWeb = ok $ toResponse $ pageTemplate
+              where pageTemplate = H.html $ do
+                                        H.head $ do
+                                            H.title (H.toHtml "leechnet")
+                                        H.body $ do
+                                            H.form ! A.action (H.toValue "../add") ! A.method (H.toValue "POST") ! A.enctype (H.toValue "multipart/form-data") $ do
+                                                H.input ! A.name (H.toValue "image") ! A.type_ (H.toValue "file")
+                                                H.input ! A.type_ (H.toValue "submit")
+                
+
+viewImage :: String -> ServerPart Response
+viewImage dataDirectory = serveDirectory DisableBrowsing [] dataDirectory
--- Main.hs
+++ Main.hs
@@ -1,13 +1,18 @@
 module Main where
 
 import Control.Monad
-import Happstack.Server (Conf(..), dirs, nullConf, simpleHTTP)
+import Happstack.Server (Conf(..), decodeBody, defaultBodyPolicy, dirs, nullConf, simpleHTTP)
 
 import Imaging
 
 serviceConf :: Conf
 serviceConf = nullConf { port = 31000 }
 
+tempDirectory = "D:\\Temp"
+dataDirectory = "D:\\Temp\\Data"
+
 main :: IO ()
-main = simpleHTTP serviceConf $ msum [ dirs "images/add" $ Imaging.addImage,
-                                       dirs "images/view" $ Imaging.viewImage ]
+main = simpleHTTP serviceConf $ do decodeBody (defaultBodyPolicy tempDirectory (10*10^6) 1000 1000)
+                                   msum [ dirs "images/add" $ Imaging.addImage dataDirectory,
+                                          dirs "images/view" $ Imaging.viewImage dataDirectory,
+                                          dirs "images/web/add" $ Imaging.addImageWeb ]
